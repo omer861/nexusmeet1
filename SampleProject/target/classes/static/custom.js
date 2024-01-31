7226fa9b6b22ad4dc1c6d10f800477ff21874584
Chandana <chandana@gamil.com>
2024-01-31 11:13:29 +0530

    updatecode

--- SampleProject/target/classes/static/custom.js
+++ SampleProject/target/classes/static/custom.js
@@ -1,4 +1,4 @@
 // local
 //const base_url = "http://localhost:8082/";
 // live
-const base_url = "https://nexusmeet.gatherhub.in/";
+const base_url = "https://nexusmeet.labyrinthglobalsolutions.com/";
